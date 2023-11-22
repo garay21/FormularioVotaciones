846f78694513fe61a88b0237676d6a2e3d0c51b2
garay21 <garaymolina21@gmail.com>
2023-11-22 18:06:15 -0300

    formulario vista completa

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
 const App = document.querySelector('#app')
 
-document.onload(
-    ()=>{
-        fetch('./../../src/view/formulario.php')
-        .then(Response => Response.text)
-        .then(HTMLElement => {
-            App.innerHTML = HTMLElement
+// Al iniciar la aplicacion se hace un fetch con ajax para 
+// pedir los datos del formulario y unirlos a la app
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetch('./../../src/view/formulario.php')
+        .then(Response => Response.text())
+        .then(element => {
+            App.innerHTML = element
         })
-        .catch(Error => console.error(Error))
-    }
-)
+});
--- src/view/formulario.php
+++ src/view/formulario.php
@@ -4,6 +4,31 @@
 
 <form action="" method="post">
     <h2>Formulario de Vatoción</h2>
-    <label for="">Nombre</label>
-    <input type="text" name="" id="" placeholder="Nombre del votante">
+    <label for="name-lastname">Nombre y apellido</label>
+    <input type="text" name="name-lastname" placeholder="Nombre y apellido del votante">
+    <label for="alias">Alias</label> 
+    <input type="text" name="alias" placeholder="Alias del votante">
+    <label for="rut">Rut</label>
+    <input type="text" name="rut" placeholder="Rut del votante">
+    <label for="e-mail">e-mail</label>
+    <input type="text" name="e-mail" placeholder="e-mail del votante">
+    <label for="">Region</label>
+    <select name="region" id="">
+        <option value="0">Seleccione una región</option>
+    </select>  
+    <label for="">Comuna</label>
+    <select name="comuna" id="">
+        <option value="0">Selecione una comuna</option>
+    </select>
+    <label for="">Candidato</label>
+    <select name="candidato" id="candidato">
+        <option value="">Selecione un candidato</option>
+    </select>
+    <label for="">Como se entero de nosotros</label>
+    <div class="check-content">
+        <label for="">Web</label><input type="checkbox" name="sobre" id="" >
+        <label for="">TV</label><input type="checkbox" name="sobre" id="">
+        <label for="">Redes sociales</label><input type="checkbox" name="sobre" id="">
+        <label for="">Amigos</label><input type="checkbox" name="sobre" id="">
+    </div>
 </form>
